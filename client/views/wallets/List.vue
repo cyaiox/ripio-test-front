b3eb1b71e6ae93896cca06ed88f944fddcd310a7
Gabriel Diaz <gabrieldiaz31@gmail.com>
2018-05-28 02:36:06 -0300

    Add view to list all wallets of account

---
+++ client/views/wallets/List.vue
@@ -0,0 +1,36 @@
+<template>
+  <article class="tile is-child box is-vertical is-3">    
+      <h4 class="title">Wallets</h4>
+      <summary-box v-for="wallet in wallets" 
+                  :wallet="wallet" 
+                  :key="wallet.id"
+                  :class="{'notification is-primary': wallet.id == value.id}"
+                  @click="click">
+      </summary-box>    
+  </article>
+</template>
+
+<script>
+  import SummaryBox from './SummaryBox'
+
+  export default {
+    props: {
+      wallets: Array,
+      value: Object
+    },
+
+    components: {
+      SummaryBox
+    },
+
+    methods: {
+      click (wallet) {
+        this.$emit('input', wallet)
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- client/views/wallets/index.vue
+++ client/views/wallets/index.vue
@@ -1,36 +1,169 @@
 <template>
-  <article class="tile is-child box is-vertical is-3">    
-      <h4 class="title">Wallets</h4>
-      <summary-box v-for="wallet in wallets" 
-                  :wallet="wallet" 
-                  :key="wallet.id"
-                  :class="{'notification is-primary': wallet.id == value.id}"
-                  @click="click">
-      </summary-box>    
-  </article>
+  <div>
+    <div class="tile is-ancestor">
+      <div class="tile is-parent">      
+        <article class="tile is-child box">
+          <div class="columns">
+            <div class="column">
+                <h4 class="title">Wallets</h4>
+            </div>
+            <div class="column is-one-fifth">
+              <button class="button is-primary" @click="newWallet">New Wallet</button>
+            </div>
+            
+          </div>
+          <section>
+            <b-table
+              :data="wallets"
+              :loading="loading"
+              striped>
+
+              <template slot-scope="props">
+                  <b-table-column field="id" label="Wallet" sortable>
+                      <strong>{{ props.row.id }}</strong>
+                  </b-table-column>
+
+                  <b-table-column field="balance" label="Balance" sortable>
+                      {{ props.row.balance }}
+                  </b-table-column>
+
+                  <b-table-column field="coin.name" label="Coin" sortable>
+                      {{ props.row.coin.name.toUpperCase() }} - {{ props.row.coin.symbol }}
+                  </b-table-column>
+              </template>
+
+              <template slot="empty">
+                <div class="has-text-centered">
+                  <b>The account doesn't cointain any wallet.</b>
+                </div>
+              </template>
+            </b-table>
+          </section>
+        </article>
+      </div>
+    </div>
+
+    <new-wallet :visible="openNewWallet" 
+                :coins="coins"
+                @close="closeNewWallet">
+    </new-wallet>
+  </div>
 </template>
 
 <script>
-  import SummaryBox from './SummaryBox'
+  import Vue from 'vue'
+  import Notification from 'vue-bulma-notification'
+  import NewWallet from './NewWallet'
+
+  const NotificationComponent = Vue.extend(Notification)
+
+  const api = 'http://localhost:8000'
+
+  const openNotification = (propsData = {
+    title: '',
+    message: '',
+    type: '',
+    direction: '',
+    duration: 4500,
+    container: '.notifications'
+  }) => {
+    return new NotificationComponent({
+      el: document.createElement('div'),
+      propsData
+    })
+  }
 
   export default {
-    props: {
-      wallets: Array,
-      value: Object
+    components: {
+      NewWallet
     },
 
-    components: {
-      SummaryBox
+    data () {
+      return {
+        coins: [],
+        wallets: [],
+        loading: false,
+        openNewWallet: false
+      }
+    },
+
+    beforeCreate () {
+      const loading = this.$loading.open({
+        container: null
+      })
+
+      this.$http({
+        url: `${api}/coins/`
+      }).then((response) => {
+        response.data.forEach(coin => this.coins.push(coin))
+
+        loading.close()
+      }).catch((error) => {
+        throw error
+      })
+    },
+
+    mounted () {
+      this.loadWallets()
     },
 
     methods: {
-      click (wallet) {
-        this.$emit('input', wallet)
+      loadWallets () {
+        this.loading = true
+
+        this.wallets = []
+
+        this.$http({
+          url: `${api}/balance/wallets/`
+        }).then((response) => {
+          response.data.forEach(wallet => this.wallets.push(wallet))
+
+          this.loading = false
+        }).catch((error) => {
+          throw error
+        })
+      },
+
+      newWallet () {
+        this.openNewWallet = true
+      },
+
+      closeNewWallet (coin) {
+        if (coin !== null) {
+          this.$http({
+            method: 'POST',
+            url: `${api}/balance/wallet/`,
+            data: coin
+          }).then((response) => {
+            if (!response.data.hasOwnProperty('error')) {
+              openNotification({
+                message: 'Your wallet has been created successfully.',
+                type: 'success',
+                duration: 3000
+              })
+              this.loadWallets()
+            } else {
+              openNotification({
+                message: 'The wallet cannot be created.',
+                type: 'danger',
+                duration: 3000
+              })
+            }
+          }).catch((error) => {
+            openNotification({
+              message: 'The wallet cannot be created.',
+              type: 'danger',
+              duration: 3000
+            })
+            throw error
+          })
+        }
+
+        this.openNewWallet = false
       }
     }
   }
 </script>
 
 <style lang="scss" scoped>
-
 </style>
